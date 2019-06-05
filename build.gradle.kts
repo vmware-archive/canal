278dafc2cd5e23167c6ec9415d16d47b8dc584e4
Clay McCoy <github@claymccoy.com>
2019-06-05 00:20:22 -0500

    allows gradle to build in IJ

--- build.gradle.kts
+++ build.gradle.kts
@@ -59,11 +59,3 @@ dependencies {
 dependencyLocking {
     lockAllConfigurations()
 }
-val compileKotlin: KotlinCompile by tasks
-compileKotlin.kotlinOptions {
-    jvmTarget = "1.8"
-}
-val compileTestKotlin: KotlinCompile by tasks
-compileTestKotlin.kotlinOptions {
-    jvmTarget = "1.8"
-}
